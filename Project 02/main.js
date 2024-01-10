1a4f8f9cbb3f4edb4e2f4b623bba37391f9ffa93
Kounain-Al-Ibad <156159422+Kounain-Al-Ibad@users.noreply.github.com>
2024-01-09 20:57:00 -0800

    Add files via upload
    string, number, boolean

---
+++ Project 02/main.js
@@ -0,0 +1,13 @@
+"use strict";
+//strongly typed syntax
+let a = "Saudia Arabia";
+a = "Jeddah";
+let b = 14;
+let c = false;
+//type inference
+let e = "Jeddah";
+let f = 20.8;
+f = 36;
+let g = true;
+g = true;
+console.log;
---
+++ Project 02/main.ts
@@ -0,0 +1,14 @@
+//strongly typed syntax
+let a : string = "Saudia Arabia";
+a = "Jeddah";
+let b : number = 14;
+let c : boolean = false;
+
+//type inference
+let e = "Jeddah";
+let f = 20.8;
+f = 36;
+let g = true;
+g = true;
+
+console.log
